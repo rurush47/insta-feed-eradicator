dc6b6938e81d49c11e5afc4f615b871dfd29ddaf
alex <alex@lichthund.com>
2020-07-01 12:59:29 +0200

    Add icon on click event

--- content.js
+++ content.js
@@ -1,2 +1,11 @@
 // content.js
-alert("Hello from your Chrome extension!")
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+    if(request.message === "clicked_browser_action")
+    {
+        var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+        console.log(firstHref);
+    }
+});
