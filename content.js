1ecc737562678639cb5110fb12678ce94a541958
alex <alex@lichthund.com>
2020-07-02 12:07:50 +0200

    Remove whole insta page on button click

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
 // content.js
+
+chrome.runtime.onMessage.addListener(function (request) 
+{
+    alert(request);
+    console.log(document.querySelector('#react-root')); 
+
+    var element = document.querySelector('#react-root');
+    element.parentElement.removeChild(element);
+});
--- popup.js
+++ popup.js
@@ -1,15 +1,13 @@
-let changeColor = document.getElementById('changeColor');
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('button').addEventListener('click', onClick, false)
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+  function onClick ()
+  {
+      chrome.tabs.query({currentWindow: true, active: true}, 
+        function (tabs)
+        {
+            chrome.tabs.sendMessage(tabs[0].id, 'hi');
+        })
+  }
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-};
+}, false);
