f93f104d08fc95d67108db055538ce58e4418733
alex <alex@lichthund.com>
2020-07-02 12:15:10 +0200

    Remove stories on button click

--- content.js
+++ content.js
@@ -2,9 +2,13 @@
 
 chrome.runtime.onMessage.addListener(function (request) 
 {
-    alert(request);
+    //alert(request);
     console.log(document.querySelector('#react-root')); 
+    console.log(document.querySelector('.zGtbP')); 
 
-    var element = document.querySelector('#react-root');
-    element.parentElement.removeChild(element);
+    var storyDiv = document.querySelector('.zGtbP');
+    storyDiv.parentElement.removeChild(storyDiv);
+
+    // var element = document.querySelector('#react-root');
+    // element.parentElement.removeChild(element);
 });
