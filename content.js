5b9b4d3ee5b1db0d8e1e8381a38f51e3cfcd31c9
alex <alex@lichthund.com>
2020-07-03 18:45:59 +0200

    Change remove to css injection

--- content.js
+++ content.js
@@ -1,21 +1,30 @@
 storySelector = '.zGtbP';
-feedSelector = '._8Rm4L';
+feedSelector = '.cGcGK div:nth-child(2)';
+postSelector = '_8Rm4L'
 loadingIconSelector = '.By4nA';
+disableString = '{display:none !important}';
 
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+}
+
+// $(document).ready(function()
+// {
+    
+// });
 
 chrome.runtime.onMessage.addListener(function (request) 
 {
     if(request == 'removeStories')
     {
-        var storyDiv = document.querySelector(storySelector);
-        storyDiv.parentElement.removeChild(storyDiv);
+        addStyle(storySelector + disableString);
     }
     if(request == 'removeFeed')
     {
-        var feedDiv = document.querySelector(feedSelector).parentElement.parentElement;
-        feedDiv.parentElement.removeChild(feedDiv);
-
-        var loadingIcon = document.querySelector(loadingIconSelector);
-        loadingIcon.parentElement.removeChild(loadingIcon);
+        addStyle(postSelector + disableString);
+        addStyle(feedSelector + disableString);
+        addStyle(loadingIconSelector + disableString);
     }
 });
